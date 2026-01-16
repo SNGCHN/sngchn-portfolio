0bcec8b9e4be16d44aed4772fd4cd10bf4f46ee8
SNGCHN <sngchn0312@naver.com>
2026-01-17 04:14:47 +0900

    chore: FSD 재정립 및 디자인 시스템 기반 구축

---
+++ src/views/home/ui/HomePage.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+/**
+ * @description 홈 페이지 조립장 (HomePage)
+ * 사용자님과 차근차근 위젯들을 조립해 나갈 예정입니다.
+ */
+export function HomePage() {
+  return (
+    <main className="min-h-screen">
+      {/* TODO: 여기서부터 사용자님과 함께 MainSection부터 채워갑니다. */}
+      <h1 className="text-huge font-bold p-12">SNGCHN</h1>
+    </main>
+  );
+}
